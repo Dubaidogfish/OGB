854b09323861fb045a033b2af59ee49810a778ca
E.Allington <om@uk-1.com>
2015-07-26 16:31:30 +0400

    altered myvisits.html.erb and pages controller.rb to only show current user pins

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,5 +9,6 @@ class PagesController < ApplicationController
   end
 
   def myvisits
+  	@pins = current_user.pins.order("created_at DESC").paginate(:page => params[:page], :per_page => 20)
   end	
 end
